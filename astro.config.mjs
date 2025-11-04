143f87ac4c5e61674d5801661f5ba4f32d6d881d
CyberSphinxxx <johnlemargonzales@gmail.com>
2025-11-04 16:55:57 +0800

    Change Initial Files to Blog Template

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,13 @@
 // @ts-check
+
+import mdx from '@astrojs/mdx';
+import sitemap from '@astrojs/sitemap';
 import { defineConfig } from 'astro/config';
 
 import react from '@astrojs/react';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react()]
+    site: 'https://example.com',
+    integrations: [mdx(), sitemap(), react()],
 });
